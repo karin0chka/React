76cd82c2dd560e82bb7fecdb92fdab3bcf0bf771
Karina <karina.kvych@gmail.com>
2023-08-06 14:34:11 +0100

    change http to https

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,16 +4,16 @@ const apiKey = `138f825d07c82965807b0b7b403a564d`;
 
 async function getCityList(city) {
   return await axios.get(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}&units=metric`
+    `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}&units=metric`
   );
 }
 async function getWeatherByLocation(lat, lon) {
   return await axios.get(
-    `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`)
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`)
 }
 async function get5DaysWeather(lat, lon) {
   return await axios.get(
-    `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
   );
 }
 
